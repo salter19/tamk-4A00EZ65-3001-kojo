e8b39c3b094c01161422aabe500ef71699188e2c
salter19 <salonenterhi19@gmail.com>
2021-01-13 15:17:04 +0200

    Create Honey component.

---
+++ Honey.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from "react-native";
+const Honey = () => {
+  return (
+    
+    <Text>Hi honey!</Text>
+    
+  );
+};
+
+export default Honey;
