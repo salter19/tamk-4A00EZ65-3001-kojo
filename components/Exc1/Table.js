c9570287d940cfbdc4a7345026c79bb87de6de13
salter19 <salonenterhi19@gmail.com>
2021-01-14 16:20:07 +0200

    Add to Table: styles. Outer View.

--- components/Exc1/Table.js
+++ components/Exc1/Table.js
@@ -3,6 +3,34 @@ import {View, Text, StyleSheet} from 'react-native';
 
 const Table = () => {
 
+  const boxRow = () => {
+    let result = [];
+    for (let i = 0; i < 3; i++) {
+      result.push(
+        <View key={i} style={singleBox}>
+
+        </View>
+      )
+    }
+  }
+
+  return (
+    <View style={styles.root}>
+      
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    width: "61%",
+    flex:1,
+    flexDirection:"row",
+    borderColor:"#fff",
+    borderWidth:2
+  },
+  singleBox: {
+  }
+});
 
 export default Table;
