f2742e7b078ed3f9508b69df7ba8c777bdb0b4b8
salter19 <salonenterhi19@gmail.com>
2021-01-14 15:49:54 +0200

    Add to TextContent: styles.

--- components/Exc1/TextContent.js
+++ components/Exc1/TextContent.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-const TextContent = () => {
+const TextContent = ({content}) => {
+
+  return (
+    <View style={styles.root}>
+      <Text style={styles.body}>{content}</Text>
+    </View>
+  );
 
 };
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: "center"
+  },
+  body: {
+    textAlign:"justify"
+  }
+});
 
 export default TextContent;
