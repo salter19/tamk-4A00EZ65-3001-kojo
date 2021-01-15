cc981d80111360947c8acaecc624444c5b855b0b
salter19 <salonenterhi19@gmail.com>
2021-01-15 15:52:16 +0200

    Refactor TextAndTable: turn into dynamic comp.

--- components/Exc1/TextAndTable.js
+++ components/Exc1/TextAndTable.js
@@ -1,12 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View } from "react-native";
-import Header from './components/Exc1/Header'
-import TextContent from './components/Exc1/TextContent'
-import Table3x3 from './components/Exc1/Table3x3'
+import Header from './Header';
+import TextContent from './TextContent';
+import Table3x3 from './Table3x3';
 
-const TextAndTable = ({header}) => {
-  const [message, setMessage] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+const TextAndTable = ({header, textContent}) => {
+  const [message, setMessage] = useState('');
 
+  useEffect(() => {
+    setMessage(textContent);
+  }, [])
 
   return(
     <View>
