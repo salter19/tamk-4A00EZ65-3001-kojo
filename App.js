d9eb2bdfcfd1b3fb2bdbda04986a9fd37d73f394
salter19 <salonenterhi19@gmail.com>
2021-01-15 15:51:31 +0200

    Add to App: import and use TextAndTable.

--- App.js
+++ App.js
@@ -4,25 +4,14 @@ import { Button, Text, View, StyleSheet } from "react-native";
 import TextAndTable from './components/Exc1/TextAndTable';
 // import InputText from './components/InputText'
 
-/*
-  const yes = () => {
-    setMessage('Tappara!');
-  };
 
-  const lectureView = () => {
-    return (
-      <View style={styles.root}>
-        <InputText onSubmitPress={yes}/>
-        <Text>{message}</Text>
-      </View>
-    );
-  };
- */
-
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const tableAndTextHeader = 'Otsikko';
 export default function App() {
 
   return (
     <View style={styles.root}>
+      <TextAndTable header={tableAndTextHeader} textContent={loremIpsum}/>
       <StatusBar style="auto" />
     </View>
   );
