c998ca39aef7862fcd592cebbf5e28152572b460
salter19 <salonenterhi19@gmail.com>
2021-01-15 14:59:23 +0200

    Refactor Table: stack table at first load. Style.

--- components/Exc1/Table.js
+++ components/Exc1/Table.js
@@ -1,35 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 const Table = () => {
 
-  const boxRow = () => {
+  const [table, setTable] = useState();
+
+  useEffect(() => {
+    setTable(stackTable());
+  }, []);
+
+  const boxRow = (rowID) => {
     let result = [];
     for (let i = 0; i < 3; i++) {
       result.push(
-        <View key={i} style={singleBox}>
-
+        <View key={i} style={styles.singleBox}>
+          {<Text style={styles.boxText}>{i + rowID}</Text>}
         </View>
-      )
-    }
+      );
+    };
+    return result;
+  }
+
+  const stackTable = () => {
+    let result = [];
+    const rowCount = 3;
+
+    console.log("stacking table")
+
+    for (let i = 0; i < rowCount; i++) {
+      result.push(
+        <View key={i} style={styles.horizontal}>
+          {boxRow(i < 3 ? i + 1 : i + 2)}
+        </View>
+      );
+    };
+    return result;
   }
 
   return (
     <View style={styles.root}>
-      
+      {table}      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-    width: "61%",
-    flex:1,
-    flexDirection:"row",
-    borderColor:"#fff",
-    borderWidth:2
+    alignItems:"center",
+    marginTop: "4.2%"   
   },
   singleBox: {
+    width: 81,
+    height: 81,
+    backgroundColor:"#0c2576",
+    borderColor: "#fff",
+    borderWidth:2,
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  horizontal: {
+    flexDirection:"row"
+  },
+  boxText: {
+    color: "#fff",
+    fontWeight: "bold"
   }
 });
 
