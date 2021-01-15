af70a57fd2d4a9dd946a858799ee1c6c520983f5
salter19 <salonenterhi19@gmail.com>
2021-01-15 16:33:57 +0200

    Comment TextAndTable.

--- components/Exc1/TextAndTable.js
+++ components/Exc1/TextAndTable.js
@@ -1,24 +1,24 @@
-import React, {useState, useEffect} from 'react';
+// imports
+import React from 'react';
 import { View } from "react-native";
 import Header from './Header';
 import TextContent from './TextContent';
 import Table3x3 from './Table3x3';
 
+// TextAndTable component takes in as props header and textContent.
+// props are destructed for the sake of more compact code.
 const TextAndTable = ({header, textContent}) => {
-  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    setMessage(textContent);
-  }, [])
 
   return(
     <View>
       <Header headerText={header}/>
-      <TextContent content={message}/>
+      <TextContent content={textContent}/>
       <Table3x3 />
     </View>
   );
 
 };
 
+// TextAndTable needs to be exported so it can be used in elsewhere.
 export default TextAndTable;
