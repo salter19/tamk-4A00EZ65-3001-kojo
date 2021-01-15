9db4f10354b576a0f87888f9c505d05364d46838
salter19 <salonenterhi19@gmail.com>
2021-01-15 15:43:06 +0200

    Add to App: import TextAndTable.

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
+import React from "react";
 import { Button, Text, View, StyleSheet } from "react-native";
+import TextAndTable from './components/Exc1/TextAndTable';
 // import InputText from './components/InputText'
-import Header from './components/Exc1/Header'
-import TextContent from './components/Exc1/TextContent'
-import Table3x3 from './components/Exc1/Table3x3'
 
 /*
   const yes = () => {
@@ -22,14 +20,9 @@ import Table3x3 from './components/Exc1/Table3x3'
  */
 
 export default function App() {
-  const [message, setMessage] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
-
 
   return (
     <View style={styles.root}>
-      <Header headerText="Otsikko"/>
-      <TextContent content={message}/>
-      <Table3x3 />
       <StatusBar style="auto" />
     </View>
   );
