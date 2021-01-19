75dc0e777809dd1cec99098ab18f4c64079ef9c8
salter19 <salonenterhi19@gmail.com>
2021-01-19 13:06:38 +0200

    Refactor App: sandboxin InputText and hooks.

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 // imports needed
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Button, Text, View, StyleSheet } from "react-native";
-import TextAndTable from './components/Exc1/TextAndTable';
-// import InputText from './components/InputText'
+import React, {useState} from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Constants from 'expo-constants';
+// import TextAndTable from './components/Exc1/TextAndTable';
+import InputText from './components/InputText';
+import ImageDisplay from './components/ImageDisplay';
 
 
 // helper variables to demonstrate dynamic nature of component(s)
@@ -13,15 +15,43 @@ const tableAndTextHeader = 'Otsikko';
 // the App to rule them all
 export default function App() {
 
+  const [message, setMessage] = useState('');
+  const [image, setImage] = useState();
+
+  const onInputTextSubmit = (input) => {
+
+    input === 'Tappara' || input === 'tappara'
+    ? setOutputWithImage()
+    : setOutput(input);
+  }
+
+  const setOutputWithImage = () => {
+    setMessage('Kaikki jotka mahtuu änkee halliin!');
+    setImage(<ImageDisplay/>);
+  }
+
+  const setOutput = (input) => {
+    setMessage(input);
+    setImage(null);
+  }
+
   return (
     <View style={styles.root}>
 
       {/* TextAndTable component includes Header, TextContent and Table3x3 components.
       One of each. */}
-      <TextAndTable header={tableAndTextHeader} textContent={loremIpsum}/>
+      {/* <TextAndTable header={tableAndTextHeader} textContent={loremIpsum}/> */}
+
+      {/* InputText is a sandbox thingy, now with image! */}
+      <InputText onSubmitPress={onInputTextSubmit}/>
+      <Text style={styles.message}>{message}</Text>
+      <View>{image}</View>
 
       {/* StatusBar component is phone's statusbar. */}
-      <StatusBar style="auto" />
+      <View style={styles.statusbar}>
+        <StatusBar style="auto" />
+      </View>
+      
     </View>
   );
 };
@@ -31,9 +61,13 @@ const styles = StyleSheet.create({
   root: {
     flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
     backgroundColor:"#ff6600",
     height:"100%",
-    paddingTop:"25%",
     paddingLeft:"5%"
-  }
+  },
+  statusbar: {
+    height: Constants.statusBarHeight,
+    
+  }, 
 });
