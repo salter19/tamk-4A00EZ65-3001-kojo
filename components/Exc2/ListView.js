21801ea829dac9e58462c2c8620e35e1975de7bf
salter19 <salonenterhi19@gmail.com>
2021-01-25 12:08:27 +0200

    Add to Listview: funtionality to add item.

--- components/Exc2/ListView.js
+++ components/Exc2/ListView.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Array, Text, View} from 'react-native';
 
-const ListView = () => {
+const ListView = ({input}) => {
 
+    const [itemsArr, setItemsArr] = useState([]);
+
+    // call useEffect at each input change
+    useEffect(() => {
+        // add input into itemsArr
+        addItemHandler();
+    }, [input]);
+    
+    const addItemHandler = () => {
+        if (input) {
+            setItemsArr([...itemsArr, input]);
+        }
+    };
+
+    return (
+        <View >
+            {itemsArr.map((e, index) => <Text key={index}>{e}</Text>)}
+        </View>
+    );
 };
 
 export default ListView;
