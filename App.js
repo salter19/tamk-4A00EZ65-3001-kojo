6f859ebb782b09f2f7b97a6d09d97c3bd775d9dd
Terhi <salonenterhi19@gmail.com>
2021-01-26 11:02:06 +0200

    Add EditTask. Automated styiling to all.

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 // imports needed
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 // import TextAndTable from './components/Exc1/TextAndTable';
-import InputText from './components/Exc2/InputText';
-import ImageDisplay from './components/ImageDisplay';
-import TextListView from './components/Exc2/TextListView';
+import InputText from "./components/Exc2/InputText";
+import ImageDisplay from "./components/ImageDisplay";
+import TextListView from "./components/Exc2/TextListView";
 
-import {v4 as uuidv4} from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 // helper variables to demonstrate dynamic nature of component(s) in components/Exc1/
 // const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
@@ -17,7 +16,6 @@ import {v4 as uuidv4} from 'uuid';
 
 // the App to rule them all
 export default function App() {
-
   const [taskArr, setTaskArr] = useState([]);
   // const [task, setTask] = useState('');
   // const [image, setImage] = useState();
@@ -41,17 +39,14 @@ export default function App() {
   //   setImage(null);
   // }
 
-
   // add tasks to an array
   const addTaskHandler = (task) => {
-    
-    setTaskArr([...taskArr, {key:uuidv4(), text:task}]);
-  }
+    setTaskArr([...taskArr, { key: uuidv4(), text: task }]);
+  };
 
   // this is the delete, which removes
   // task pressed by its id
   const deleteItem = (itemKey) => {
-
     // iterate / work on a temp
     let tmp = taskArr;
 
@@ -68,31 +63,29 @@ export default function App() {
 
     // set the temp into taskArr
     setTaskArr(tmp);
-  }
+  };
 
   return (
     <View style={styles.root}>
-
       {/* TextAndTable component includes Header, TextContent and Table3x3 components.
       One of each. */}
       {/* <TextAndTable header={tableAndTextHeader} textContent={loremIpsum}/> */}
 
       {/* InputText for text input */}
-      <InputText onSubmitPress={addTaskHandler}/>
+      <InputText onSubmitPress={addTaskHandler} buttonTitle="OK" />
       {/* <Text style={styles.message}>{task}</Text> */}
-      
+
       {/* TextListView  to list those tasks*/}
-      <TextListView input={taskArr} deleteItem={deleteItem}/>
+      <TextListView input={taskArr} deleteItem={deleteItem} />
       {/* <View>{image}</View> */}
 
       {/* StatusBar component is phone's statusbar. */}
       <View style={styles.statusbar}>
         <StatusBar style="auto" />
       </View>
-      
     </View>
   );
-};
+}
 
 // Styling for elements in App
 const styles = StyleSheet.create({
@@ -100,11 +93,11 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor:"#ff6600",
-    height:"100%",
-    paddingLeft:"5%"
+    backgroundColor: "#ff6600",
+    height: "100%",
+    paddingLeft: "5%",
   },
   statusbar: {
-    height: Constants.statusBarHeight,    
-  }, 
+    height: Constants.statusBarHeight,
+  },
 });
---
+++ components/EditTask.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Modal, StyleSheet, View, Button } from "react-native";
+
+import InputText from "./Exc2/InputText";
+
+const EditTask = (props) => {
+  const onClosePressed = () => {
+    console.log("Close modal");
+  };
+  return (
+    <Modal animationType="fade" transparent={true} visible={props.visible}>
+      <View style={styles.root}>
+        {/*ToDo: dynamic title */}
+        <InputText
+          onSubmitPressed={props.onSubmitPressed}
+          buttonTitle="Close"
+        />
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: "rgba(255, 255, 255, 0.75)",
+    borderColor: "#0c2576",
+    borderWidth: 4,
+    borderRadius: 2,
+  },
+});
+
+export default EditTask;
