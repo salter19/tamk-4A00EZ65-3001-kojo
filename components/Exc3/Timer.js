0387ed5ca7cada25c1a76f8443357f4bb0f125e7
salter19 <salonenterhi19@gmail.com>
2021-01-29 15:06:11 +0200

    Add functionality to Timer.

--- components/Exc3/Timer.js
+++ components/Exc3/Timer.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
 
-const Timer = () => {
+const Timer = ({secondsGone, timeIsUp}) => {
+
+  const [time, setTime] = useState(secondsGone);
+  const [isCounting, setCounting] = useState(true);
+
+  // useEffect has to values in the second param
+  // secondsGone makes sure the object re-renders by each second 
+  // while isCounting makes sure the rendering stops when it is
+  // set to false.  
+  useEffect(() => {
+
+    if (isCounting) {
+      
+      // arbitrary time limit 
+      // for future: 
+      // take in props for the limit, instant stop & and counter start
+      if (time <= 50 -1 && time >= 0) {
+
+        // tic toc goes the clock
+        timeHandler();
+      } else {
+        // timeIsUp() is func given in props
+        timeIsUp();
+
+        // boolean to stop the rendering
+        setCounting(false);
+      }
+    }
+    
+  }, [secondsGone, isCounting]);
+
+  const timeHandler = () => {
+    setTime(secondsGone);
+  }
+  
+
     return (
-        <View></View>
+        <View>
+          <Text>Tic toc goes the clock: {time} seconds gone</Text>
+        </View>
     );
-}
+};
+
+export default Timer;
