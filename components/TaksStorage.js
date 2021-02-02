21f1d81dafc93c7322bc3c4a105a4c00d8f5fe88
salter19 <salonenterhi19@gmail.com>
2021-02-02 10:59:55 +0200

    Add TaksStorage.js. Empty template. Todo:typo in name.

---
+++ components/TaksStorage.js
@@ -0,0 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const STORAGE_KEY = 'Tasks';
+
+const LoadTasks = () => {
+    // TODO: give innerts
+};
+
+const SaveTasks = () => {
+    // TODO: give innerts
+};
+
+export default {LoadTasks, SaveTasks};
