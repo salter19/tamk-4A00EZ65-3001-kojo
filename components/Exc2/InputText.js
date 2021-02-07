6aae924069090afeab11375215dbf1bab190696a
salter19 <salonenterhi19@gmail.com>
2021-02-07 20:29:52 +0200

    Refactor InputText: remove fieldtype references.

--- components/Exc2/InputText.js
+++ components/Exc2/InputText.js
@@ -1,67 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { Text, TextInput, Button, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 
 const InputText = (props) => {
   const placeholder = "Fill in";
-  const titles = ['Task title', 'Description', 'Date'];
   const [userInput, setUserInput] = useState("");
-  const [fieldTitle, setFieldTitle] = useState(undefined);
-
-  const onSubmit = () => {
-
-    if (userInput.length > 0 && userInput !== " ") {
-      props.onSubmitPress(userInput);
-      setUserInput("");
-    }
-  };
 
   // send user input to EditTask as it changes
   const handleOnChangeText = (event) => {
     // show on field
     setUserInput(event);
 
-    // send data to EditTask if field is known
-    if (props.fieldtype !== undefined) {
-      if (props.currentItem !== undefined) {
-        props.updateData({ 
-          key:props.currentItem.key, 
-          fieldtype:props.fieldtype, 
-          value:event
-        });
-      } else {
-        props.updateData({
-          fieldtype: props.fieldtype,
-          value:event
-        });
-      }
-    }
-
+    // update at edit
+    props.updateData(event); 
   }
 
   // set field value, while on update view (!== add new task view)
   useEffect(() => {
 
     if (props.currentItem !== undefined) {
-      setUserInput(props.currentItem.title);
+      setUserInput(props.currentItem)
     } else {
-      setUserInput("")
+      setUserInput("");
     }
     
   }, [props.currentItem]);
 
-  // set field title if one is given
-  useEffect(() => {
-    if (props.fieldtype != undefined) {
-      if (props.fieldtype <= titles.length -1) {
-        setFieldTitle(titles[props.fieldtype]);
-      }        
-    }
-  
-  }, [])
-
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>{fieldTitle}</Text>
+      {console.log(userInput)}
 
       <View style={styles.row}>
         <TextInput
