d199b2618d103c57d5b36ad4d01851dffa705241
salter19 <salonenterhi19@gmail.com>
2021-02-14 18:15:46 +0200

    Add CameraComponent: singAlong w/ Kojo.

--- components/CameraComponent.js
+++ components/CameraComponent.js
@@ -1,147 +1,156 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useRef} from 'react';
 import {View, StyleSheet, Text, 
-  TouchableOpacity, Dimensions, Platform, Modal} from 'react-native';
-import {Camera} from 'expo-camera';
+  TouchableOpacity, Modal, Image} from 'react-native';
+import {Camera as CameraView} from 'expo-camera';
+import * as FileSystem from 'expo-file-system';
 
-const CameraComponent = ({onCloseCamera}) => {
+import { IMAGE_DIR, CAMERA_BACK, CAMERA_FRONT, WINDOW_WIDTH } from '../data/Constants';
+
+
+const CameraComponent = ({onCloseCamera, isVisible}) => {
   // camera permissions
   const [hasPermission, setHasPermission] = useState(undefined);
-  const [camera, setCamera] = useState(null);
 
-  // set camera type, screen ratio and image padding
-  const [type, setType] = useState(Camera.Constants.Type.back);
-//   const [isRatioSet, setIsRatioSet] = useState(false);
-//   const [ratio, setRatio] = useState('4:3');
-//   const {height, width} = Dimensions.get('screen');
-//   const screenRatio = height / width;
-//   const [imgPadding, setImgPadding] = useState(0);  
+  // set camera type, latest picture
+  const [type, setType] = useState(CAMERA_BACK);
+  const [latestPic, setLatestPic] = useState(undefined);
 
-  // get permission to use camera
-  // ToDo: why does this not ask persmission?
-  useEffect(() => {
+  // useRefs
+  const cameraRef = useRef(undefined);
+
+  const onShowCamera = () => {
     (async() => {
-      const {status} = await Camera.requestPermissionsAsync().catch(e => console.log(e));
-      setHasPermission(status === 'granted');
+        const {status} = await CameraView.requestPermissionsAsync().catch(e => console.log(e));
+        setHasPermission(status === 'granted');
     })();
-  }, []);
+  };
+  
+  // flip between front and back camera
+  const toggleCameras = () => {
+    setType(
+      type === CAMERA_BACK
+        ? CAMERA_FRONT
+        : CAMERA_BACK
+    );
+  }
+
+  // snap the image
+  const takePicture = () => {   
+    (async() => {
+      if (cameraRef !== undefined) {
+        let photo = await cameraRef.current.takePictureAsync().catch(e => console.error(e));
+
+        // move taken picture to permanent location
+        let imgFolderInfo = await FileSystem.getInfoAsync(IMAGE_DIR).catch(e => console.error(e));
+        if (!imgFolderInfo.exists) {
+
+          // create directory if it does not exist
+          // options: if intermediates === true, create all none existing sub folders
+          await FileSystem.makeDirectoryAsync(IMAGE_DIR, {intermediates: true}).catch(e => console.error(e));
+        }
+
+        const fileName = Date.now().toString() + ".jpg";
+        const fullPath = IMAGE_DIR + fileName;
+
+        console.log(`from${photo.uri} to:${fullPath}`);
+        await FileSystem.moveAsync({from: photo.uri, to: fullPath}).catch(e => console.error(e));
+        setLatestPic(fullPath);
+      }
+    })();
+  };
 
   // handle !== haspermission situations
   if (hasPermission === undefined) {
     return (
-        <Modal onShow={}>
-            <View style={styles.info}>
-                <Text>Waiting for permission to use camera.</Text>
-            </View>
+      <Modal 
+        onShow={onShowCamera}
+        animationType="fade"
+        visible={isVisible}
+        onRequestClose={onCloseCamera}
+      >
+        <View style={styles.info}>
+          <Text>Waiting for permission to use camera.</Text>
+
+          <View style={styles.button}>
+            <TouchableOpacity onPress={onCloseCamera}>
+              <Text>close view</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+    </Modal>
+    );
+
+  } else if (hasPermission === false) {
+    return (
+      <Modal
+        animationType="fade"
+        visible={isVisible}
+        onRequestClose={onCloseCamera}
+      >
+        <View style={styles.info}>
+          <Text>Camera access denied.</Text>
+
+          <View style={styles.button}>
+            <TouchableOpacity onPress={onCloseCamera}>
+              <Text>close view</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </Modal>
     );
-  }
-
-  const onShowCamera = () => {
-      (async () => {
-          await CameraComponent.re
-      })()
-  }
-
-  if (hasPermission === false) {
+  } else {
     return (
-      <View style={styles.info}>
-        <Text>Camera access denied</Text>
-      </View>
-    );
-  }
+      <Modal
+        animationType="fade"
+        visible={isVisible}
+        onRequestClose={onCloseCamera}
+      >
+        <View style={styles.root}>
 
-  // flip between front and back camera
-  const handleTypeChoice = () => {
-    setType(
-      type === Camera.Constants.Type.back
-        ? Camera.Constants.Type.front
-        : Camera.Constants.Type.back
-    );
-  }
-
-  // load camera for use
-  const setCameraReady = async() => {
-    if (!isRatioSet) {
-      await prepareRatio();
-    }
-  };
-
-  // set camera ratio and padding (assumes portrait mode)
-  const prepareRatio = async() => {
-    // default given is system default
-    let desiredRatio = '4:3';
-
-    // something little for android only
-    if (Platform.OS === 'android') {
-      const ratios = await camera.getSupportedRatioAsync().catch(e => console.log(e));
-
-      // calculate w / h of each camera ratio
-      // NOTE: these are measured in landscape mode
-      // find ratio closest to the screen ratio (don't go over)
-      let distances = {};
-      let realRatios = {};
-      let minDistance = null;
-
-      for (let ratio of ratios) {
-        const parts = ratio.split(':');
-        const realRatio = parseInt(parts[0]) / parseInt(parts[1]);
-        realRatios[ratio] = realRatio;
-
-        // ratio can't be taller than screen, so no for abs()
-        const distance = screenRatio - realRatio;
-        distances[ratio] = realRatio;
-        if (minDistance === null) {
-          minDistance = ratio;
-        } else {
-          if (distance >= 0 && distance < distances[minDistance]) {
-            minDistance = ratio;
-          }
-        }
-      }
-      // set best match
-      desiredRatio = minDistance;
-
-      // calculate the diff between cam width and screen height
-      const remainder = Math.floor(
-        (height - realRatios[desiredRatio] * width) /2
-      );
-
-      // set the preview padding and ratio
-      setImgPadding(remainder);
-      setRatio(desiredRatio);
-
-      // set flag to prevent re-calculation w/ each rendering
-      setIsRatioSet(true);
-    }
-  }
-
-
-  return (
-    <Modal>
-      <View style={styles.root}>
-        <Camera 
-          style= {[styles.cameraPreview, {marginVertical: imgPadding}]}
-          type={type}
-          onCameraReady={setCameraReady}
-          ratio={ratio}
-          ref={(ref) => setCamera(ref)}
-        >
-          <View style={}>
-            <TouchableOpacity onPress={handleTypeChoice}>
-              <Text>Flip Camera</Text>
-            </TouchableOpacity>
-          </View>
           <View>
-            <TouchableOpacity onPress={onCloseCamera}>
-              <Text>Close Camera</Text>
-            </TouchableOpacity>
+            {/* -> */}
+            <CameraView 
+              style={styles.cameraPreview}
+              type={type}
+              ref={cameraRef}
+            >
+            </CameraView>
           </View>
 
-        </Camera>
+          <View style={styles.bottomButtonContainer}>
 
-      </View>
-    </Modal>
-  );
+            <View style={styles.button}>
+                <TouchableOpacity onPress={toggleCameras}>
+                  <Text>flip camera</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.button}>
+                <TouchableOpacity onPress={takePicture}>
+                  <Text>OH SNAP!</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.button}>
+              <TouchableOpacity onPress={onCloseCamera}>
+                <Text>close view</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+
+          <View style={styles.latest}>
+            <Image source={{uri:latestPic}}  style={styles.prevPic}/>
+          </View>
+
+         
+
+        
+        </View>
+      </Modal>
+    );
+
+  }
 };
 
 const styles = StyleSheet.create({
@@ -158,7 +167,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   cameraPreview: {
-    flex:1,
+    width: WINDOW_WIDTH,
+    height: WINDOW_WIDTH * (4 / 3),
+  },
+  bottomButtonContainer: {
+    marginTop: 3,
+    flexDirection:"row",
+    width:"100%",
+    height:100,
+    backgroundColor:"#ff6600",
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  button: {
+    borderColor:"#ff6600",
+    width: 100,
+    color:"#ffffff",
+  },
+  latest: {
+    flexDirection:"row",
+    justifyContent:"center",
+  },
+  prevPic: {
+    width: 120,
+    height: 90,
   }
 });
 
