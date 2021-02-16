dacef2ba8a60dfa462bbf74fe8777aeaab30518c
salter19 <salonenterhi19@gmail.com>
2021-02-16 11:04:47 +0200

    Add to Gallery: image preview.

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import {View, Button, Image, Modal, StyleSheet, Text} from 'react-native';
 
+import {WINDOW_WIDTH} from './../data/Constants';
 
-
+const PADDED_WIDTH = WINDOW_WIDTH * 0.8;
 
 const Gallery = (props) => {
   const [picGallery, setPicGallery] = useState([]);
@@ -28,6 +29,18 @@ const Gallery = (props) => {
     }
   }, [picGallery]);
 
+  // set current pic according to navigation
+  useEffect(() => {
+    if (picGallery.length > 0) {
+
+      setCurrentPic({
+        index: picGallery[picIndex].index,
+        path: picGallery[picIndex].path
+      })
+    }
+  }, [picIndex]);
+
+  // button functionality
   const moveNext = () => {
     if (picIndex < picGallery.length -1) {
       const tmp = picIndex + 1;
@@ -35,6 +48,7 @@ const Gallery = (props) => {
     }
   };
 
+  // button functionality
   const movePrev = () => {
     if (picIndex > 0) {
       const tmp = picIndex -1;
@@ -42,14 +56,22 @@ const Gallery = (props) => {
     }
   }
 
-  console.log('pic gallery, current pic index')
-  console.log(picIndex + ' | ' + (picGallery.length -1))
   return (
     <Modal>
 
-      <View>
-        <Text>ToDo: show images here</Text>
+      <View style={styles.topHeader}>
+        <Text style={styles.header}>Picture Gallery</Text>
       </View>
+
+      { currentPic.path !== undefined 
+        ? <View style={styles.latest}>
+            <Image source={{uri:currentPic.path}}  style={styles.prevPic}/>
+          </View>
+        : <View>
+            <Text> To Do: loading animation</Text>
+          </View>
+      }
+      
 
       <View style={styles.row}>
         <Button title="prev" onPress={movePrev}/>
@@ -67,6 +89,28 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:'center',
     marginTop: "2%",
+  },
+  latest: {
+    flexDirection:"row",
+    justifyContent:"center",
+  },
+  prevPic: {
+    width: PADDED_WIDTH,
+    height: PADDED_WIDTH * ( 4 / 3 ),
+    borderColor:"#0c2567",
+    borderWidth: 8,
+    borderRadius:6,
+  },
+  topHeader:{
+    justifyContent:"center",
+    alignItems:"center",
+    paddingHorizontal: "2%",
+    paddingVertical: "3%"
+  },
+  header: {
+    fontSize:42,
+    fontWeight:"bold",
+    textAlign:"center",
   },
 });
 export default Gallery;
