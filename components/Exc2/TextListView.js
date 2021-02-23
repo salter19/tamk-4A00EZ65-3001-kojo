112d0d9a7d2bae69790e5bdaabe7105463173e57
salter19 <salonenterhi19@gmail.com>
2021-02-23 10:26:00 +0200

    No change.

--- components/Exc2/TextListView.js
+++ components/Exc2/TextListView.js
@@ -1,160 +1,154 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    Text, 
-    StyleSheet, 
-    TouchableOpacity, 
-    View,
-    FlatList,
-    SafeAreaView
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  FlatList,
+  SafeAreaView,
 } from 'react-native';
 
 // my components
 import ImageDisplay from '../ImageDisplay';
-import {formatDateTime, formatDateTimeFromDate} from '../utils';
+import { formatDateTime, formatDateTimeFromDate } from '../utils';
 
 // static width wrapper for text
 const wrap = (str) => {
-  const res = str.replace(new RegExp(`(?![^\\n]{1,24}$)([^\\n]{1,24})\\s`, 'g'),'$1\n');
+  const res = str.replace(
+    new RegExp(`(?![^\\n]{1,24}$)([^\\n]{1,24})\\s`, 'g'),
+    '$1\n',
+  );
   return res;
-}
+};
 
 const format = (dateObj) => {
   // type tester
-  const str = "this a string";
+  const str = 'this a string';
 
   if (typeof dateObj !== typeof str) {
-    // handle Date object    
+    // handle Date object
     return formatDateTimeFromDate(dateObj);
   } else {
     return formatDateTime(dateObj);
   }
-}
+};
 
-const TextListView = ({tasksArr, deleteItem, modifyItem}) => {
-    const [deleteI, setDeleteI] = useState(null);
+const TextListView = ({ tasksArr, deleteItem, modifyItem }) => {
+  const [deleteI, setDeleteI] = useState(null);
 
-    const _onPress = (item) => {
-      modifyItem(item);
-    };
+  const _onPress = (item) => {
+    modifyItem(item);
+  };
 
-    const _onLongPress = (item) => {
-        // set state deleteI
-        // it is used to update the flatlist
-        setDeleteI(item.key);
+  const _onLongPress = (item) => {
+    // set state deleteI
+    // it is used to update the flatlist
+    setDeleteI(item.key);
 
-        // call deleteItem in App
-        deleteItem(item.key);
-    };
+    // call deleteItem in App
+    deleteItem(item.key);
+  };
 
-    const renderItem = ({item, index}) => {
-
-      return (
-        
-        <TouchableOpacity 
-            key={item.key} 
-            onPress={() => _onPress(item)}
-            onLongPress= { () => _onLongPress(item)}
-        >
-          <View style={[
-            index % 2 === 0 
-            ? [styles.touchableItem_even, styles.listItem]
-            : styles.listItem,
-            ]}>
-
-              <View style={styles.rowItem}>
-                <ImageDisplay />
-                <View style={styles.columnItem}>
-
-                  <View>
-                    <Text style={[styles.text, styles.titleText]}>
-                      {wrap(item.title)}
-                    </Text>
-                  </View>
-                  
-                  <View>
-                    <Text style={[styles.text, styles.descriptionText]}>
-                      {wrap(item.description)}
-                    </Text>
-                  </View>
-
-                  <View>
-                    <Text style={styles.text}>
-                      {format(item.date)}
-                    </Text>
-                  </View>
-
-                  <View>
-                    <Text style={styles.text}>
-                      {'latitude: ' + item.location.latitude}
-                    </Text>
-                    <Text style={styles.text}>
-                      {'longitude: ' + item.location.longitude}
-                    </Text>
-                  </View>
-
-                </View> 
-              </View>
-              
-          </View>          
-            
-        </TouchableOpacity>);   
-    }
-
-    const itemSeparator = () => {
-        return (
-            <View style={styles.separator}/>
-        );
-    }
-    
+  const renderItem = ({ item, index }) => {
     return (
-        <SafeAreaView style={styles.container}>
-          {/* FlatList for scrolling */}
-          <FlatList 
-            style={styles.flatListView}
-            data={tasksArr}
-            renderItem={renderItem}
-            extraData={deleteI}
-            ItemSeparatorComponent={itemSeparator}
-          >
-          <Text>{deleteI}</Text>
-          </FlatList>
-        </SafeAreaView>
+      <TouchableOpacity
+        key={item.key}
+        onPress={() => _onPress(item)}
+        onLongPress={() => _onLongPress(item)}
+      >
+        <View
+          style={[
+            index % 2 === 0
+              ? [styles.touchableItem_even, styles.listItem]
+              : styles.listItem,
+          ]}
+        >
+          <View style={styles.rowItem}>
+            <ImageDisplay />
+            <View style={styles.columnItem}>
+              <View>
+                <Text style={[styles.text, styles.titleText]}>
+                  {wrap(item.title)}
+                </Text>
+              </View>
+
+              <View>
+                <Text style={[styles.text, styles.descriptionText]}>
+                  {wrap(item.description)}
+                </Text>
+              </View>
+
+              <View>
+                <Text style={styles.text}>{format(item.date)}</Text>
+              </View>
+
+              <View>
+                <Text style={styles.text}>
+                  {'latitude: ' + item.location.latitude}
+                </Text>
+                <Text style={styles.text}>
+                  {'longitude: ' + item.location.longitude}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
     );
-   
+  };
+
+  const itemSeparator = () => {
+    return <View style={styles.separator} />;
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* FlatList for scrolling */}
+      <FlatList
+        style={styles.flatListView}
+        data={tasksArr}
+        renderItem={renderItem}
+        extraData={deleteI}
+        ItemSeparatorComponent={itemSeparator}
+      >
+        <Text>{deleteI}</Text>
+      </FlatList>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-      width: "100%",
-      paddingHorizontal: "2%",
-      borderWidth: 4,
-      borderColor: "#0c2567",
-      borderRadius: 2,
-      marginVertical:"3%",
-      alignItems:"center",
-      backgroundColor:"#ff6600",
+    width: '100%',
+    paddingHorizontal: '2%',
+    borderWidth: 4,
+    borderColor: '#0c2567',
+    borderRadius: 2,
+    marginVertical: '3%',
+    alignItems: 'center',
+    backgroundColor: '#ff6600',
   },
   flatListView: {
-      marginVertical:"8%",
-      width: "97%",
+    marginVertical: '8%',
+    width: '97%',
   },
   text: {
-      color:"#ffffff", 
+    color: '#ffffff',
   },
   titleText: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 24,
     lineHeight: 30,
-    paddingTop: "2%"
+    paddingTop: '2%',
   },
   descriptionText: {
     fontSize: 18,
     lineHeight: 24,
-    flexWrap:"wrap",
+    flexWrap: 'wrap',
   },
   listItem: {
-    width: "100%",
-    borderColor: "#ffffff",
+    width: '100%',
+    borderColor: '#ffffff',
     borderWidth: 4,
     borderRadius: 2,
     marginVertical: 2,
@@ -162,24 +156,24 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
   },
   touchableItem_even: {
-    backgroundColor:"#0c2576",
+    backgroundColor: '#0c2576',
   },
   separator: {
     height: 8,
-    width:"80%",
-    backgroundColor: "#0c2576",
-    alignSelf:"center",
-    borderTopColor:"#ffffff",
-    borderBottomColor:"#ffffff",
-    borderWidth:2,
-    marginVertical:4,
+    width: '80%',
+    backgroundColor: '#0c2576',
+    alignSelf: 'center',
+    borderTopColor: '#ffffff',
+    borderBottomColor: '#ffffff',
+    borderWidth: 2,
+    marginVertical: 4,
   },
   rowItem: {
-    flexDirection:'row',
+    flexDirection: 'row',
   },
   columnItem: {
-    flexDirection:'column'
-  }
+    flexDirection: 'column',
+  },
 });
 
 export default TextListView;
