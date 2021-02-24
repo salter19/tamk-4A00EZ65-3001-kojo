6a9bb451b6823ab8fe6aa7bd13fc19d10724ef29
salter19 <salonenterhi19@gmail.com>
2021-02-24 11:00:15 +0200

    Add to ButtonBase: innards.

--- components/ButtonBase.js
+++ components/ButtonBase.js
@@ -1,8 +1,42 @@
 import React from 'react';
-import {View, Text, Pressable} from 'react-native';
+import {Text, Pressable, StyleSheet} from 'react-native';
 
-const ButtonBase = () => {
+const ButtonBase = ({onPress, buttonText}) => {
+
+  return (
+    <Pressable
+            onPress={onPress}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed
+                  ? 'rgba(12, 37, 103, 0.81)'
+                  : 'rgba(12, 37, 103, 1)',
+              },
+              styles.myButton,
+            ]}
+    >
+      <Text style={styles.buttonText}>{buttonText}</Text>
+    </Pressable>
+  );
 
 }
+
+const styles = StyleSheet.create(
+{  myButton: {
+    width: 100,
+    height: 61,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '10%',
+    marginHorizontal: '5%',
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#fff',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '700',
+  },}
+)
 
 export default ButtonBase;
