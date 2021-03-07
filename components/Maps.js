177ecd44905af3da552816c52d95a5515237e3d8
salter19 <salonenterhi19@gmail.com>
2021-03-07 15:52:18 +0200

    Add Maps: empty template.

---
+++ components/Maps.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Modal, Text} from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const Maps = (props) => {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+export default Maps;
